51aa50b3c848578d4fefa93ddd559eecd2c0a3f9
Marc <teste74@hotmail.fr>
2022-10-02 20:47:53 +0200

    Update start-stop.sh

--- ASUSTOR/AppCentral/Emby/CONTROL/start-stop.sh
+++ ASUSTOR/AppCentral/Emby/CONTROL/start-stop.sh
@@ -4,3 +4,24 @@
 CONTAINER_NAME=Emby
 
 #########################################################################################################################################################
+case "$1" in
+  start)
+    docker start $CONTAINER_NAME
+    sleep 3
+    ;;
+  stop)
+    docker stop $CONTAINER_NAME
+    sleep 3
+    ;;
+  reload)
+    docker stop $CONTAINER_NAME
+    sleep 3
+    docker start $CONTAINER_NAME
+    ;;
+  *)
+		echo "Usage: $0 {start|stop|reload}"
+    exit 1
+    ;;
+esac
+exit 0
+

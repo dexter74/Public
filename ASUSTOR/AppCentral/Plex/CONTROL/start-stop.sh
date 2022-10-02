e118f80eb02ca79cac6b1432d6794200cf8a5298
Marc <teste74@hotmail.fr>
2022-10-02 17:40:42 +0200

    Update start-stop.sh

--- ASUSTOR/AppCentral/Plex/CONTROL/start-stop.sh
+++ ASUSTOR/AppCentral/Plex/CONTROL/start-stop.sh
@@ -1,10 +1,6 @@
 #############################################################################################
 CONTAINER_NAME=Plex
-ADM_SSL_FILE=/usr/builtin/etc/certificate/ssl.pem
-PORTAINER_SSL_FILE=/share/Docker/PortainerCE/data/certs/cert.pem
-PORTAINER_KEY_FILE=/share/Docker/PortainerCE/data/certs/key.pem
 #############################################################################################
-CERT_DIFF=$(diff -bBq $ADM_SSL_FILE $PORTAINER_SSL_FILE | grep differ | awk '{print $5}')
 #############################################################################################
 case "$1" in
 	start)
@@ -17,10 +13,6 @@ case "$1" in
 	stop)
 		echo "Stop $CONTAINER_NAME container..."
 		docker stop $CONTAINER_NAME
-		if [ "$CERT_DIFF" == "differ" ]; then
-			rm -f $PORTAINER_SSL_FILE
-			rm -f $PORTAINER_KEY_FILE
-		fi
 		sleep 3
 		;;
     
@@ -28,16 +20,11 @@ case "$1" in
 	reload)
   	echo "Reload $CONTAINER_NAME container..."		
 		docker stop  $CONTAINER_NAME
-			if [ "$CERT_DIFF" == "differ" ]; then
-				rm -f $PORTAINER_SSL_FILE
-				rm -f $PORTAINER_KEY_FILE
-			fi
 		sleep 3
     docker start $CONTAINER_NAME
     sleep 2
 		;;
 		
-    
 	*)
 		echo "Usage: $0 {start|stop|reload}"
     exit 1

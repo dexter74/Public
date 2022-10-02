f08369665dab4dcf84e8b6f2c29974da56ba40c5
Marc <teste74@hotmail.fr>
2022-10-02 19:20:23 +0200

    Update post-install.sh

--- ASUSTOR/AppCentral/Emby/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Emby/CONTROL/post-install.sh
@@ -1,1 +1,21 @@
+#!/bin/sh
 
+APP_DIR=/usr/local/AppCentral/emby-server
+
+case "$APKG_PKG_STATUS" in
+
+  install)
+    getent passwd emby | grep data > /dev/null 2>&1 && userdel emby
+    getent group emby > /dev/null 2>&1 || groupadd -r emby
+    getent passwd emby > /dev/null 2>&1 || useradd -r -c 'Emby Server' -g emby -d /home/emby -m -s /bin/false emby && passwd -l emby > /dev/null
+    ;;
+  upgrade)
+    getent passwd emby | grep data > /dev/null 2>&1 && userdel emby
+    getent passwd emby > /dev/null 2>&1 || useradd -r -c 'Emby Server' -g emby -d /home/emby -m -s /bin/false emby && passwd -l emby > /dev/null
+    ;;
+  *)
+    ;;
+
+esac
+
+exit 0

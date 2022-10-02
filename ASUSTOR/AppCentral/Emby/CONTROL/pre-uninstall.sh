f2d88bfdb3b9a723d51dc128280b2f111629a400
Marc <teste74@hotmail.fr>
2022-10-02 17:44:01 +0200

    Update pre-uninstall.sh

--- ASUSTOR/AppCentral/Emby/CONTROL/pre-uninstall.sh
+++ ASUSTOR/AppCentral/Emby/CONTROL/pre-uninstall.sh
@@ -1,1 +1,22 @@
+#!/bin/sh
+
+echo "pre-uninstall"
+
+container=$(docker container ls -a | grep Emby |awk '{print $1}')
+im=$(docker images | grep Emby | grep latest | awk '{print $3}')
+
+echo $container
+echo $im
+
+if [ ! -z $container ]; then 
+	docker kill $container
+	sleep 2
+	docker rm -f $container
+fi
+
+if [ ! -z $im ]; then 
+	if [ "$APKG_PKG_STATUS" == "uninstall" ]; then
+		docker rmi -f $im
+	fi
+fi
 

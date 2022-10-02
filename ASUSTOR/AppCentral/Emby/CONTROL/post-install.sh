8e7c61c354651747f0a957c3bdc87222be77c0e3
Marc <teste74@hotmail.fr>
2022-10-02 17:35:04 +0200

    Update post-install.sh

--- ASUSTOR/AppCentral/Emby/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Emby/CONTROL/post-install.sh
@@ -1,1 +1,15 @@
+docker stop Emby
 
+
+docker run -d --net=bridge \
+  --name Emby \
+  --restart unless-stopped \
+  -v '/share/Docker/Emby:/config' \
+  -v '/share/Video/:/Video' \
+  --device=/dev/dri:/dev/dri \
+  -p 8096:8096 \
+  -e UID=1000 \
+  -e GID=999 \
+  -e GIDLIST=100 \
+  --label cacher="oui" \
+emby/embyserver

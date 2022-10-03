dd5fbc2b899c8d26aadf4c60c557d2e0023357f4
Marc <teste74@hotmail.fr>
2022-10-03 21:03:44 +0200

    Update start-stop.sh

--- ASUSTOR/AppCentral/Portainer/CONTROL/start-stop.sh
+++ ASUSTOR/AppCentral/Portainer/CONTROL/start-stop.sh
@@ -1,1 +1,23 @@
-
+######################################################
+case "$1" in
+  start)
+    docker start Portainer
+    sleep 3
+    ;;
+  stop)
+    docker stop Portainer
+    sleep 3
+    ;;
+  reload)
+    docker stop Portainer
+    sleep 3
+    docker start Portainer
+    ;;
+  *)
+   echo "Usage: $0 {start|stop|reload}"
+   exit 1
+   ;;
+   
+esac
+exit 0
+######################################################

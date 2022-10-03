116e84222e91e09ac424b26588563db5860eab56
Marc <teste74@hotmail.fr>
2022-10-03 20:42:52 +0200

    Update post-install.sh

--- ASUSTOR/AppCentral/Emby/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Emby/CONTROL/post-install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-docker run -d --name embyserver --volume /share/Docker/Emby:/config --net=host --publish 8096:8096 --publish 8920:8920 emby/embyserver:latest
+docker run -d \
+ --name embyserver
+ --volume /share/Docker/Emby:/config \
+ --net=host \
+ --publish 8096:8096 \
+ --publish 8920:8920 \
+ emby/embyserver:latest
 
 # --env UID=1000 \ # The UID to run emby as (default: 2)
 # --env GID=100 \ # The GID to run emby as (default 2)

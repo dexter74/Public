2cb7f0d3c39b6ef96226dd160246f9dd54545992
Marc <teste74@hotmail.fr>
2022-10-03 20:44:11 +0200

    Update start-stop.sh

--- ASUSTOR/AppCentral/Emby/CONTROL/start-stop.sh
+++ ASUSTOR/AppCentral/Emby/CONTROL/start-stop.sh
@@ -1,1 +1,25 @@
 #!/bin/sh
+
+######################################################
+case "$1" in
+  start)
+    docker start Emby
+    sleep 3
+    ;;
+  stop)
+    docker stop Emby
+    sleep 3
+    ;;
+  reload)
+    docker stop Emby
+    sleep 3
+    docker start Emby
+    ;;
+  *)
+   echo "Usage: $0 {start|stop|reload}"
+   exit 1
+   ;;
+   
+esac
+exit 0
+######################################################

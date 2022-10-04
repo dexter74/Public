31e7a20d14bdc2f9ddefc752d645aac410a89ded
Marc <teste74@hotmail.fr>
2022-10-04 17:47:23 +0200

    Update post-install.sh

--- ASUSTOR/AppCentral/Plex/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Plex/CONTROL/post-install.sh
@@ -1,29 +1,29 @@
 ##########################################################################################################################################################
 # Déclaration de la variable de test #
 ######################################
-CONTAINER_NAME=Plex
-IMAGE=linuxserver/plex
-HTTP=32400:32400
+#CONTAINER_NAME=Plex
+#IMAGE=linuxserver/plex
+#HTTP=32400:32400
 
 ##########################################################################################################################################################
 # Fermeture du Conteneur 
-docker container rm -f  $CONTAINER_NAME
+#docker container rm -f  $CONTAINER_NAME
 
 ##########################################################################################################################################################
 # Lancement du Conteneur
-docker run -d \
---name=$CONTAINER_NAME \
---volume /volume1/Docker/$CONTAINER_NAME:/config \
---volume /volume1/Video:/Video \
---net=host \
---publish $HTTP \
---device /dev/dri:/dev/dri \
---restart unless-stopped \
---label cacher="oui" \
-$IMAGE:latest
+#docker run -d \
+#--name=$CONTAINER_NAME \
+#--volume /volume1/Docker/$CONTAINER_NAME:/config \
+#--volume /volume1/Video:/Video \
+#--net=host \
+#--publish $HTTP \
+#--device /dev/dri:/dev/dri \
+#--restart unless-stopped \
+#--label cacher="oui" \
+#$IMAGE:latest
 
 ##########################################################################################################################################################
-docker start $CONTAINER_NAME
+#docker start $CONTAINER_NAME
 
 
 ##########################################################################################################################################################

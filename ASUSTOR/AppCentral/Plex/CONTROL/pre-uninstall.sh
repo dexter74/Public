9da8e8d4c2b971c9575c6c3807b0118ea63bfa0a
Marc <teste74@hotmail.fr>
2022-10-04 17:49:40 +0200

    Update pre-uninstall.sh

--- ASUSTOR/AppCentral/Plex/CONTROL/pre-uninstall.sh
+++ ASUSTOR/AppCentral/Plex/CONTROL/pre-uninstall.sh
@@ -1,22 +1,21 @@
-#!/bin/sh
 ######################################################################################
-CONTENEUR=Plex
-container=$(docker container ls -a | grep $CONTENEUR |awk '{print $1}')
-im=$(docker images | grep $container | grep latest | awk '{print $3}')
+#CONTENEUR=Plex
+#container=$(docker container ls -a | grep $CONTENEUR |awk '{print $1}')
+#im=$(docker images | grep $container | grep latest | awk '{print $3}')
 ######################################################################################
-echo "pre-uninstall"
-echo $container
-echo $im
+#echo "pre-uninstall"
+#echo $container
+#echo $im
 ######################################################################################
-if [ ! -z $container ]; then 
-	docker kill $container
-	sleep 2
-	docker rm -f $container
-fi
+#if [ ! -z $container ]; then 
+#	docker kill $container
+#	sleep 2
+#	docker rm -f $container
+#fi
 ######################################################################################
-if [ ! -z $im ]; then 
-	if [ "$APKG_PKG_STATUS" == "uninstall" ]; then
-		docker rmi -f $im
-	fi
-fi
+#if [ ! -z $im ]; then 
+#	if [ "$APKG_PKG_STATUS" == "uninstall" ]; then
+#		docker rmi -f $im
+#	fi
+#fi
 ######################################################################################

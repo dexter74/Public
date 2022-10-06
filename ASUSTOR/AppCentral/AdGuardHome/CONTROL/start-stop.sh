5c1f8d496e843a624f97e253b00a99090ca6eb3b
Marc <teste74@hotmail.fr>
2022-10-06 16:38:19 +0200

    Create start-stop.sh

---
+++ ASUSTOR/AppCentral/AdGuardHome/CONTROL/start-stop.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+##########################################################################################################################################################
+# Déclaration de Variable #
+###########################
+CONTENEUR=AdGuardHome
+
+case "$1" in
+  start)
+    docker start $CONTENEUR
+    sleep 3
+    ;;
+  stop)
+    docker stop $CONTENEUR
+    sleep 3
+    ;;
+  reload)
+    docker stop $CONTENEUR
+    sleep 3
+    docker start $CONTENEUR
+    ;;
+  *)
+   echo "Usage: $0 {start|stop|reload}"
+   exit 1
+   ;;
+   
+esac
+exit 0
+##########################################################################################################################################################

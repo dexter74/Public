6729d246ede17b41efcb05a50977859e49892dd6
Marc <teste74@hotmail.fr>
2022-10-07 21:18:50 +0200

    Update start.sh

--- ASUSTOR/AppCentral/start.sh
+++ ASUSTOR/AppCentral/start.sh
@@ -1,11 +1,14 @@
+################################################################################
 cd ~/APPZ/ASUSTOR/AppCentral/AdGuardHome && apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Cloud9 && apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Emby && apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Jackett && apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Jdownloader && apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Plex && apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Portainer && apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/QBitorrent && apkg-tools_py2.py create .
+
+################################################################################
+cd ~/APPZ/ASUSTOR/AppCentral/Cloud9 && apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Jackett && apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Jdownloader && apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Radarr && apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/ReverseProxy && apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Sonarr && apkg-tools_py2.py create .

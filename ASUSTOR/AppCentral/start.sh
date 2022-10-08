48cb6848480cbe6ea39c9d4ff764d4129c6821b2
Marc <teste74@hotmail.fr>
2022-10-08 05:17:43 +0200

    Update start.sh

--- ASUSTOR/AppCentral/start.sh
+++ ASUSTOR/AppCentral/start.sh
@@ -1,14 +1,14 @@
 ########################################################################################
-# cd ~/APPZ/ASUSTOR/AppCentral/AdGuardHome   		&& apkg-tools_py2.py create .
-# cd ~/APPZ/ASUSTOR/AppCentral/Emby         		&& apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/AdGuardHome   		&& apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Bitwarden     		&& apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Emby         		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/MariaDB      		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/NginxReverseProxy 	&& apkg-tools_py2.py create .
-# cd ~/APPZ/ASUSTOR/AppCentral/Plex          		&& apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Plex          		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/phpMyAdmin    		&& apkg-tools_py2.py create .
-# cd ~/APPZ/ASUSTOR/AppCentral/Portainer     		&& apkg-tools_py2.py create .
-# cd ~/APPZ/ASUSTOR/AppCentral/QBitorrent    		&& apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Portainer     		&& apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/QBitorrent    		&& apkg-tools_py2.py create .
 ########################################################################################
-cd ~/APPZ/ASUSTOR/AppCentral/Bitwarden     		&& apkg-tools_py2.py create .
 ########################################################################################
 # cd ~/APPZ/ASUSTOR/AppCentral/Cloud9       && apkg-tools_py2.py create .
 # cd ~/APPZ/ASUSTOR/AppCentral/Jackett      && apkg-tools_py2.py create .

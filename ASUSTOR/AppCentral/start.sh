948b8cd6bc720cbba3820e72497a2613a82f2bf9
Marc <teste74@hotmail.fr>
2022-10-08 08:21:23 +0200

    up

--- ASUSTOR/AppCentral/start.sh
+++ ASUSTOR/AppCentral/start.sh
@@ -5,8 +5,10 @@
 # Tentative réussi
 cd ~/APPZ/ASUSTOR/AppCentral/AdGuardHome   		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Bitwarden     		&& apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Cloud9       		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Dozzle       		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Emby         		&& apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/FileBrowser 		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/MariaDB      		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Nexclipper    		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/NginxReverseProxy 	&& apkg-tools_py2.py create .
@@ -15,8 +17,6 @@ cd ~/APPZ/ASUSTOR/AppCentral/phpMyAdmin    		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Portainer     		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/QBitorrent    		&& apkg-tools_py2.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Visual_Studio 		&& apkg-tools_py2.py create .
-########################################################################################
-cd ~/APPZ/ASUSTOR/AppCentral/Cloud9       && apkg-tools_py2.py create .
 ########################################################################################
 #
 #

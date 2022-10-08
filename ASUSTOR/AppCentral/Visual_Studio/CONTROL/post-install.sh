833abc5e4fc4e2c047a2c5042ac55e3e565a2527
Marc <teste74@hotmail.fr>
2022-10-08 06:18:20 +0200

    up

--- ASUSTOR/AppCentral/Visual_Studio/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Visual_Studio/CONTROL/post-install.sh
@@ -18,18 +18,17 @@ docker container rm -f  $CONTENEUR
 docker run -d \
 --name=$CONTENEUR \
 --restart unless-stopped \
---net=host \
+--net=bridge \
 --hostname $CONTENEUR \
 --volume /volume1/Docker/$CONTENEUR:/config \
 --env TZ="Europe/Paris" \
 --env PASSWORD="password" \
---env HASHED_PASSWORD="" \
---env \
---env \
+--env HASHED_PASSWORD="6b3a55e0261b0304143f805a24924d0c1c44524821305f31d9277843b8a10f4e" \
+--env SUDO_PASSWORD="admin" \
+--env SUDO_PASSWORD_HASH="fc8252c8dc55839967c58b9ad755a59b61b67c13227ddae4bd3f78a38bf394f7" \
+--env PROXY_DOMAIN="http://localhost" \
+--env  DEFAULT_WORKSPACE="/config/workspace" \
 --publish $HTTP \
-
-
-
 --label cacher="oui" \
 $IMAGE:latest
 

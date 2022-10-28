119a5bffa72db80c17c0ee5053a02f42d8c7b139
Marc <teste74@hotmail.fr>
2022-10-28 16:05:06 +0200

    Update post-install.sh

--- ASUSTOR/AppCentral/Plex/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Plex/CONTROL/post-install.sh
@@ -17,7 +17,7 @@ docker container rm -f  $CONTENEUR
 ##########################
 docker run -d \
 --name=$CONTENEUR \
---restart unless-stopped \
+--restart always \
 --net=host \
 --hostname $CONTENEUR \
 --volume /volume1/Docker/$CONTENEUR:/config \

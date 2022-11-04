ca3eb5c854682bdbc0833f80f0ffea8da7c218b1
Marc <teste74@hotmail.fr>
2022-11-04 20:06:45 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/AdGuardHome/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/AdGuardHome/CONTROL/post-install.sh
@@ -19,7 +19,7 @@ docker run -d \
 --name=$CONTENEUR \
 --hostname $CONTENEUR \
 --net=host \
---restart none \
+--restart no \
 --env TZ="Europe/Paris" \
 --volume /volume1/Docker/$CONTENEUR/conf:/opt/adguardhome/conf \
 --volume /volume1/Docker/$CONTENEUR/work:/opt/adguardhome/work \

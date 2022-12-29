f061a09b557607ef6e4d92a951efb0af4a1cfe1b
Marc <teste74@hotmail.fr>
2022-12-29 18:04:12 +0100

    Update pre-uninstall.sh

--- ASUSTOR/AppCentral/AdGuardHome/CONTROL/pre-uninstall.sh
+++ ASUSTOR/AppCentral/AdGuardHome/CONTROL/pre-uninstall.sh
@@ -3,12 +3,26 @@
 ##########################################################################################################################################################
 # Déclaration de Variable #
 ###########################
-CONTENEUR=AdGuardHome
-container_id=$()
-docker container ls -a | grep $CONTENEUR |awk '{print $1}'
-im=$(docker images | grep $container | grep latest | awk '{print $3}')
+container=$(docker container ls -a | grep AdGuardHome | awk '{print $1}')
+im=$(docker images | grep adguard/adguardhome | grep latest | awk '{print $3}')
 
 ##########################################################################################################################################################
-# Phrase de désinstallation #
-#############################
+# Message de Sortie #
+#####################
 echo "pre-uninstall"
+echo $container
+echo $im
+
+
+##########################################################################################################################################################
+# Suppression du conteneur #
+############################
+if [ ! -z $container ]; then
+	docker rm -f $container
+fi
+
+if [ ! -z $im ]; then
+	if [ "$APKG_PKG_STATUS" == "uninstall" ]; then
+		docker rmi -f $im
+	fi
+fi

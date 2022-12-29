60f1ed461528846938a5ab8ab1403f4463322f06
Marc <teste74@hotmail.fr>
2022-12-29 15:59:15 +0100

    Update Custom.sh

--- ASUSTOR/AppCentral/Custom.sh
+++ ASUSTOR/AppCentral/Custom.sh
@@ -4,12 +4,14 @@ docker start qBittorrent
 docker start Emby
 docker start MariaDB
 docker start ReverseProxy
-docker start bitwarden" > docker_start.sh
+docker start bitwarden
+docker start PyLoad" > docker_start.sh
 
 echo "clear;
 docker stop bitwarden
 docker stop ReverseProxy
 docker stop MariaDB
+docker stop PyLoad
 docker stop qBittorrent
 docker stop Emby
 docker stop AdGuardHome" > docker_stop.sh

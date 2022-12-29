c65308b6ce60f73c29fb3bbaf4e13f2787232427
Marc <teste74@hotmail.fr>
2022-12-29 14:59:35 +0100

    Update start.sh

--- ASUSTOR/AppCentral/start.sh
+++ ASUSTOR/AppCentral/start.sh
@@ -5,33 +5,33 @@
 # nano ~/APPZ/ASUSTOR/AppCentral/NordVPN/CONTROL/post-install.sh;
 
 ########################################################################################
-cd ~/APPZ/ASUSTOR/AppCentral/AdGuardHome   		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Bitwarden     		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Cloud9       		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Dozzle       		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Emby         		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/FileBrowser 		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/LibreSpeed		 	&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/MariaDB      		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Nexclipper    		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/NginxReverseProxy   	&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Plex          		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/phpMyAdmin    		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Portainer     		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Pyload/CONTROL 	&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/QBitorrent    		&& apkg-tools_py2.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Visual_Studio 		&& apkg-tools_py2.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/AdGuardHome   		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Bitwarden     		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Cloud9       		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Dozzle       		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Emby         		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/FileBrowser 		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/LibreSpeed		 	&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/MariaDB      		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Nexclipper    		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/NginxReverseProxy   	&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Plex          		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/phpMyAdmin    		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Portainer     		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Pyload/CONTROL 	&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/QBitorrent    		&& apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Visual_Studio 		&& apkg-tools_py3.py create .
 ########################################################################################
 
 #
 ########################################################################################
 # En Developpement
-# cd ~/APPZ/ASUSTOR/AppCentral/Jackett      && apkg-tools_py2.py create .
-# cd ~/APPZ/ASUSTOR/AppCentral/Jdownloader  && apkg-tools_py2.py create .
-# cd ~/APPZ/ASUSTOR/AppCentral/Radarr       && apkg-tools_py2.py create .
-# cd ~/APPZ/ASUSTOR/AppCentral/Sonarr       && apkg-tools_py2.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/Jackett      && apkg-tools_py3.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/Jdownloader  && apkg-tools_py3.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/Radarr       && apkg-tools_py3.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/Sonarr       && apkg-tools_py3.py create .
 
 ########################################################################################
 # Tentative KO
-# cd ~/APPZ/ASUSTOR/AppCentral/NordVPN		&& apkg-tools_py2.py create .
-# cd ~/APPZ/ASUSTOR/AppCentral/Wireguard	&& apkg-tools_py2.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/NordVPN		&& apkg-tools_py3.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/Wireguard	&& apkg-tools_py3.py create .

0a0df4ed896823000c8c4b1b99e65036cb0b67a8
Marc <teste74@hotmail.fr>
2022-12-29 22:20:08 +0100

    Update start-stop.sh

--- ASUSTOR/AppCentral/AdGuardHome/CONTROL/start-stop.sh
+++ ASUSTOR/AppCentral/AdGuardHome/CONTROL/start-stop.sh
@@ -9,28 +9,31 @@
 # Déclaration de Variable #
 ###########################
 APKG_PKG_DIR=/usr/local/AppCentral/AdGuardHome
-
 CONTENEUR=AdGuardHome
 
 case "$1" in
   start)
     docker start $CONTENEUR
-    sleep 3
+    sleep 5
     ;;
+
   stop)
     docker stop $CONTENEUR
-    sleep 3
+    sleep 5
     ;;
+
   reload)
     docker stop $CONTENEUR
-    sleep 3
+    sleep 5
     docker start $CONTENEUR
     ;;
+
   *)
    echo "Usage: $0 {start|stop|reload}"
    exit 1
    ;;
    
 esac
+
 exit 0
 ##########################################################################################################################################################

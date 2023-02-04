a7b5829733671e710313567f7fdc0e4e7e26386d
Marc <teste74@hotmail.fr>
2023-02-04 10:33:51 +0000

    Update start.sh

--- ASUSTOR/AppCentral/start.sh
+++ ASUSTOR/AppCentral/start.sh
@@ -17,11 +17,11 @@ cd ~/APPZ/ASUSTOR/AppCentral/LibreSpeed         && apkg-tools_py3.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/MariaDB            && apkg-tools_py3.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Nexclipper         && apkg-tools_py3.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/NginxReverseProxy 	&& apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Plex && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/phpMyAdmin && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Portainer && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/QBitorrent && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Visual_Studio && apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Plex               && apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/phpMyAdmin         && apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Portainer          && apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/QBitorrent         && apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Visual_Studio      && apkg-tools_py3.py create .
 ########################################################################################
 # En cours de test:
  cd ~/APPZ/ASUSTOR/AppCentral/Jdownloader         && apkg-tools_py3.py create .

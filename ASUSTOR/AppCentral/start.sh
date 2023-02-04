fed15b3c578d04862907eca0966ebdcc5594d9b9
Marc <teste74@hotmail.fr>
2023-02-04 10:33:33 +0000

    Update start.sh

--- ASUSTOR/AppCentral/start.sh
+++ ASUSTOR/AppCentral/start.sh
@@ -5,11 +5,11 @@
 # nano ~/APPZ/ASUSTOR/AppCentral/NordVPN/CONTROL/post-install.sh;
 
 ########################################################################################
-#cd ~/APPZ/ASUSTOR/AppCentral/Exemple    && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/AdGuardHome && apkg-tools_py3.py create .
-#cd ~/APPZ/ASUSTOR/AppCentral/Base       && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Bitwarden   && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Cloud9      && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/Exemple           && apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/AdGuardHome        && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/Base              && apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Bitwarden          && apkg-tools_py3.py create .
+cd ~/APPZ/ASUSTOR/AppCentral/Cloud9             && apkg-tools_py3.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Dozzle             && apkg-tools_py3.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/Emby               && apkg-tools_py3.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/FileBrowser        && apkg-tools_py3.py create .

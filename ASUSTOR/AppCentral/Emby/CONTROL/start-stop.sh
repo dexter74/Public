65f113a8e3ace43e888256d708d4ed18a1f8712e
Marc <teste74@hotmail.fr>
2023-02-18 23:00:19 +0100

    Update start-stop.sh

--- ASUSTOR/AppCentral/Emby/CONTROL/start-stop.sh
+++ ASUSTOR/AppCentral/Emby/CONTROL/start-stop.sh
@@ -1,29 +1,43 @@
 #!/bin/sh
 
-##########################################################################################################################################################
-# Arrêt / Relance / Mise à jour #
-#################################
-case "$1" in
-  start)
-    docker start Emby
-    sleep 3
-    ;;
-  stop)
-    docker stop Emby
-    sleep 3
-    ;;
-  reload)
-    docker stop Emby
-    sleep 3
-    docker start Emby
-    ;;
-  *)
-   echo "Usage: $0 {start|stop|reload}"
-   exit 1
-   ;;
-esac
 
 ##########################################################################################################################################################
-# Code retour de fermeture #
+# Déclaration de Variable #
+###########################
+CONTENEUR=Emby
+
+##########################################################################################################################################################
+# Message d'information #
+#########################
+echo "start-stop"
+
+##########################################################################################################################################################
+# Start-Stop #
+##############
+case "$1" in
+    start)
+        echo "Start $CONTENEUR container..."
+        docker start $CONTENEUR
+        sleep 3
+        ;;
+    stop)
+    	echo "Stop $CONTENEUR container..."
+    	docker stop $CONTENEUR
+    	sleep 3
+        ;;
+    reload)
+    	echo "Reload $CONTENEUR container..."
+    	docker stop  $CONTENEUR
+    	sleep 3
+    	docker start $CONTENEUR
+    	sleep 3
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|reload}"
+        exit 1
+        ;;
+esac
+##########################################################################################################################################################
+# Code retour de Fermeture #
 ############################
 exit 0

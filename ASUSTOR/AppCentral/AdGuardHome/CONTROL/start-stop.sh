5ef24904644f99816416bf42e21279b72def2924
Marc <teste74@hotmail.fr>
2023-02-18 20:50:30 +0100

    Update start-stop.sh

--- ASUSTOR/AppCentral/AdGuardHome/CONTROL/start-stop.sh
+++ ASUSTOR/AppCentral/AdGuardHome/CONTROL/start-stop.sh
@@ -4,28 +4,28 @@
 # Déclaration de Variable #
 ###########################
 CONTENEUR=AdGuardHome
-
 case "$1" in
-    start)
-        echo "Start $CONTAINER_NAME container..."
-        docker start $CONTAINER_NAME
-        sleep 3 
-        ;;
-    stop)
-    	echo "Stop $CONTAINER_NAME container..."
-    	docker stop $CONTAINER_NAME
-    	sleep 3
-        ;;
-    reload)
-    	echo "Reload $CONTAINER_NAME container..."
-    	docker stop  $CONTAINER_NAME
-    	sleep 3
-    	docker start $CONTAINER_NAME
-    	sleep 3
-        ;;
-    *)
-        echo "Usage: $0 {start|stop|reload}"
-        exit 1
-        ;;
+  start)
+    sleep 3
+    docker start $CONTENEUR
+    sleep 5
+    ;;
+
+  stop)
+    docker stop $CONTENEUR
+    sleep 5
+    ;;
+  reload)
+    docker stop $CONTENEUR
+    sleep 5
+    docker start $CONTENEUR
+    ;;
+  *)
+   echo "Usage: $0 {start|stop|reload}"
+   exit 1
+   ;;
+   
 esac
+
 exit 0
+##########################################################################################################################################################

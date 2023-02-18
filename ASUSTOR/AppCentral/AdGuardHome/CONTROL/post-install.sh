36235ce6844aef82319e6e4bcb7741ffdb87748d
Marc <teste74@hotmail.fr>
2023-02-18 20:21:07 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/AdGuardHome/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/AdGuardHome/CONTROL/post-install.sh
@@ -26,23 +26,14 @@ docker create -i -t \
 --volume /volume1/Docker/$CONTENEUR/work:/opt/adguardhome/work \
 --publish 53:53/tcp     \
 --publish 53:53/udp     \
---publish 67:67/udp     \
 --publish 80:80/tcp     \
 --publish 443:443/tcp   \
 --publish 443:443/udp   \
 --publish 3000:3000/tcp \
---publish 853:853/tcp   \
---publish 784:784/udp   \
---publish 853:853/udp   \
---publish 8853:8853/udp \
---publish 5443:5443/tcp \
---publish 5443:5443/udp \
 $IMAGE:latest
 
 
 # --net=host \
-#--publish 68:68/udp     \
-
 
 ##########################################################################################################################################################
 # Lancement du Conteneur #

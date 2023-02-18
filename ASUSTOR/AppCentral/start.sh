d89beeee39ed8c64a1356ce24f694fd8b99d0e53
Marc <teste74@hotmail.fr>
2023-02-18 19:25:41 +0100

    Update start.sh

--- ASUSTOR/AppCentral/start.sh
+++ ASUSTOR/AppCentral/start.sh
@@ -5,27 +5,27 @@
 #cd ~/APPZ/ASUSTOR/AppCentral/Exemple           && apkg-tools_py3.py create .
 cd ~/APPZ/ASUSTOR/AppCentral/AdGuardHome        && apkg-tools_py3.py create .
 #cd ~/APPZ/ASUSTOR/AppCentral/Base              && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Bitwarden          && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Cloud9             && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Dozzle             && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Emby               && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/FileBrowser        && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/LibreSpeed         && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/MariaDB            && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Nexclipper         && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/NginxReverseProxy 	&& apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Plex               && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/phpMyAdmin         && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Portainer          && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/QBittorrent        && apkg-tools_py3.py create .
-cd ~/APPZ/ASUSTOR/AppCentral/Visual_Studio      && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/Bitwarden          && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/Cloud9             && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/Dozzle             && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/Emby               && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/FileBrowser        && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/LibreSpeed         && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/MariaDB            && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/Nexclipper         && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/NginxReverseProxy 	&& apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/Plex               && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/phpMyAdmin         && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/Portainer          && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/QBittorrent        && apkg-tools_py3.py create .
+#cd ~/APPZ/ASUSTOR/AppCentral/Visual_Studio      && apkg-tools_py3.py create .
 ########################################################################################
 # Container in Testing
- cd ~/APPZ/ASUSTOR/AppCentral/Flaresolverr      && apkg-tools_py3.py create .
- cd ~/APPZ/ASUSTOR/AppCentral/Jdownloader       && apkg-tools_py3.py create .
- cd ~/APPZ/ASUSTOR/AppCentral/Jackett           && apkg-tools_py3.py create .
- cd ~/APPZ/ASUSTOR/AppCentral/Radarr            && apkg-tools_py3.py create .
- cd ~/APPZ/ASUSTOR/AppCentral/Sonarr            && apkg-tools_py3.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/Flaresolverr      && apkg-tools_py3.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/Jdownloader       && apkg-tools_py3.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/Jackett           && apkg-tools_py3.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/Radarr            && apkg-tools_py3.py create .
+# cd ~/APPZ/ASUSTOR/AppCentral/Sonarr            && apkg-tools_py3.py create .
 
 ########################################################################################
 # Container Edit and Ready for Deploy
@@ -36,8 +36,6 @@ cd ~/APPZ/ASUSTOR/AppCentral/Visual_Studio      && apkg-tools_py3.py create .
 # nano ~/APPZ/ASUSTOR/AppCentral/NOIP/CONTROL/post-install.sh;
 # cd ~/APPZ/ASUSTOR/AppCentral/NOIP             && apkg-tools_py3.py create .
 # --------------------------------------------------------------------------------------
-
-
 ########################################################################################
 # En Developpement
 # cd ~/APPZ/ASUSTOR/AppCentral/Pyload/          && apkg-tools_py3.py create .

67aae7f225dca9c44f20e503ab7a0265088a3567
Marc <teste74@hotmail.fr>
2023-02-18 20:14:50 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/AdGuardHome/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/AdGuardHome/CONTROL/post-install.sh
@@ -20,14 +20,11 @@ docker create -i -t \
 --hostname $CONTENEUR \
 --net=host \
 --restart $RESTART \
---subnet=192.168.0.0/24 \
 --env TZ="Europe/Paris" \
 --volume /usr/builtin/etc/certificate:/sslcerts/:ro \
 --volume /etc/localtime:/etc/localtime:ro \
 --volume /volume1/Docker/$CONTENEUR/conf:/opt/adguardhome/conf \
 --volume /volume1/Docker/$CONTENEUR/work:/opt/adguardhome/work \
---publish 3000:3000 \
---publish 3272:80 \
 $IMAGE:latest
 
 

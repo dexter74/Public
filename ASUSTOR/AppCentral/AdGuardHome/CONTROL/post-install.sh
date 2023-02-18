271c4d9371f6a8a5b59b652a9fe5edaf0e8eaed5
Marc <teste74@hotmail.fr>
2023-02-18 20:37:10 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/AdGuardHome/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/AdGuardHome/CONTROL/post-install.sh
@@ -18,6 +18,7 @@ docker container rm -f $CONTENEUR
 docker create -i -t \
 --name=$CONTENEUR \
 --hostname $CONTENEUR \
+--net=bridge \
 --restart $RESTART \
 --env TZ="Europe/Paris" \
 --volume /usr/builtin/etc/certificate:/sslcerts/:ro \

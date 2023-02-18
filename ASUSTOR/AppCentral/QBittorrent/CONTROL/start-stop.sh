b806ba02503a9e64abe285dc9943f8cb5068f6d4
Marc <teste74@hotmail.fr>
2023-02-18 23:15:09 +0100

    Update start-stop.sh

--- ASUSTOR/AppCentral/QBittorrent/CONTROL/start-stop.sh
+++ ASUSTOR/AppCentral/QBittorrent/CONTROL/start-stop.sh
@@ -4,31 +4,37 @@
 # Déclaration de Variable #
 ###########################
 CONTENEUR=qBittorrent
+##########################################################################################################################################################
+# Message d'information #
+#########################
+echo "start-stop"
 
 ##########################################################################################################################################################
 # Start-Stop #
 ##############
 case "$1" in
-  start)
-    docker start $CONTENEUR
-    sleep 3
-    ;;
-  stop)
-    docker stop $CONTENEUR
-    mv "/volume1/.@plugins/AppCentral/qBittorrent/CONTROL/qBittorrent.conf" "/share/Docker/Qbitorrent/qBittorrent/qBittorrent.conf" 2>/dev/null
-    sleep 3
-    ;;
-  reload)
-    docker stop $CONTENEUR
-    sleep 3
-    docker start $CONTENEUR
-    ;;
-  *)
-   echo "Usage: $0 {start|stop|reload}"
-   exit 1
-   ;;
+    start)
+        echo "Start $CONTENEUR container..."
+        docker start $CONTENEUR
+        sleep 3
+        ;;
+    stop)
+    	echo "Stop $CONTENEUR container..."
+    	docker stop $CONTENEUR
+    	sleep 3
+        ;;
+    reload)
+    	echo "Reload $CONTENEUR container..."
+    	docker stop  $CONTENEUR
+    	sleep 3
+    	docker start $CONTENEUR
+    	sleep 3
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|reload}"
+        exit 1
+        ;;
 esac
-
 ##########################################################################################################################################################
 # Code retour de Fermeture #
 ############################

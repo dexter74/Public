ebe97b316882cbe3f5c3fa7366046e288d7c6b2c
Marc <teste74@hotmail.fr>
2023-02-19 18:49:46 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/Bitwarden/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Bitwarden/CONTROL/post-install.sh
@@ -34,7 +34,7 @@ docker container rm -f $CONTENEUR
 ##########################################################################################################################################################
 # Lancement du Conteneur #
 ##########################
-docker run -d \
+docker create -i -t \
 --name=$CONTENEUR \
 --restart $RESTART \
 --net=bridge \

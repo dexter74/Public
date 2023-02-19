0ac75ba6c10c7b6a8168795d8120ac64c4813ea2
Marc <teste74@hotmail.fr>
2023-02-19 18:52:53 +0100

    Update start-stop.sh

--- ASUSTOR/AppCentral/QBittorrent/CONTROL/start-stop.sh
+++ ASUSTOR/AppCentral/QBittorrent/CONTROL/start-stop.sh
@@ -21,6 +21,7 @@ case "$1" in
     stop)
     	echo "Stop $CONTENEUR container..."
     	docker stop $CONTENEUR
+        mv "/volume1/.@plugins/AppCentral/qBittorrent/CONTROL/qBittorrent.conf" "/share/Docker/Qbitorrent/qBittorrent/qBittorrent.conf" 2>/dev/null
     	sleep 3
         ;;
     reload)

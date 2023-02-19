07f279c2c040f9fe0d2aceff10bb362a39e567cf
Marc <teste74@hotmail.fr>
2023-02-19 18:34:44 +0100

    Update start-stop.sh

--- ASUSTOR/AppCentral/Flaresolverr/CONTROL/start-stop.sh
+++ ASUSTOR/AppCentral/Flaresolverr/CONTROL/start-stop.sh
@@ -6,28 +6,36 @@
 CONTENEUR=Flaresolverr
 
 ##########################################################################################################################################################
+# Message d'information #
+#########################
+echo "start-stop"
+
+##########################################################################################################################################################
 # Start-Stop #
 ##############
 case "$1" in
-  start)
-    docker start $CONTENEUR
-    sleep 3
-    ;;
-  stop)
-    docker stop $CONTENEUR
-    sleep 3
-    ;;
-  reload)
-    docker stop $CONTENEUR
-    sleep 3
-    docker start $CONTENEUR
-    ;;
-  *)
-   echo "Usage: $0 {start|stop|reload}"
-   exit 1
-   ;;
+    start)
+        echo "Start $CONTENEUR container..."
+        docker start $CONTENEUR
+        sleep 3
+        ;;
+    stop)
+    	echo "Stop $CONTENEUR container..."
+    	docker stop $CONTENEUR
+    	sleep 3
+        ;;
+    reload)
+    	echo "Reload $CONTENEUR container..."
+    	docker stop  $CONTENEUR
+    	sleep 3
+    	docker start $CONTENEUR
+    	sleep 3
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|reload}"
+        exit 1
+        ;;
 esac
-
 ##########################################################################################################################################################
 # Code retour de Fermeture #
 ############################

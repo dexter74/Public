cbf51b135f3be9d86806baee936c80f66f5e3e44
Marc <teste74@hotmail.fr>
2023-02-20 01:12:52 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/Wireguard/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Wireguard/CONTROL/post-install.sh
@@ -6,7 +6,7 @@
 CONTENEUR=wireguard
 IMAGE=linuxserver/wireguard
 RESTART=unless-stopped
-PORT=51820:51820
+PORT=51820:51820/UDP
 
 ##########################################################################################################################################################
 # Fermeture du Conteneur #
@@ -16,19 +16,25 @@ docker container rm -f  $CONTENEUR
 ##########################################################################################################################################################
 # Lancement du Conteneur #
 ##########################
-docker create -i -t \
---name=$CONTENEUR \
---restart $RESTART \
---net=host \
---cap-add="NET_ADMIN" \
---hostname $CONTENEUR \
---volume /volume1/Docker/$CONTENEUR:/config \
---publish $PORT \
---label cacher="oui" \
+docker create -i -t                                                    \
+--name=$CONTENEUR                                                      \
+--hostname $CONTENEUR                                                  \
+--net=bridge                                                           \
+--dns 192.168.1.1                                                      \
+--restart $RESTART                                                     \
+--env PUID="1000"                                                      \
+--env PGID="100"                                                       \
+--env TZ="EUROPE\PAris"                                                \
+--cap-add="NET_ADMIN"                                                  \
+--sysctl net.ipv4.ip_forward=1                                         \
+--sysctl net.ipv4.conf.all.src_valid_mark=1                            \
+--volume /volume1/Docker/$CONTENEUR:/config                            \
+--volume /usr/local/AppCentral/adm-kernel-extensions/lib/:/lib/modules \
+--publish $PORT                                                        \
+--label cacher="oui"                                                   \
 $IMAGE:latest
 
 # --cap-add="SYS_MODULE" \
-#--env \
 
 ##########################################################################################################################################################
 # Démarrage du Conteneur #

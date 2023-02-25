e71a556a9321d8f5f842fbc6639d45474dedc397
Marc <teste74@hotmail.fr>
2023-02-26 00:18:08 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/OnlyOffice/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/OnlyOffice/CONTROL/post-install.sh
@@ -1,1 +1,42 @@
 
+#!/bin/sh
+
+##########################################################################################################################################################
+# Déclaration de la variable de test #
+######################################
+CONTENEUR=OnlyOffice
+IMAGE=onlyoffice/documentserver
+HTTP=1121
+RESTART=unless-stopped
+IP_NAS=$(ip add | grep 192.168.1 | cut -d "/" -f 1 | cut -c 10-30)
+
+##########################################################################################################################################################
+# Fermeture du Conteneur #
+##########################
+docker container rm -f  $CONTENEUR
+
+##########################################################################################################################################################
+# Lancement du Conteneur #
+##########################
+docker create -i -t \
+--name=$CONTENEUR \
+--restart $RESTART \
+--net=bridge \
+--hostname $CONTENEUR \
+--volume /volume1/Docker/$CONTENEUR/logs:/var/log/onlyoffice \
+--volume /volume1/Docker/$CONTENEUR/Data:/var/www/onlyoffice/Data  \
+--publish $HTTP:80 \
+$IMAGE:latest
+
+#--env \
+#--volume /var/log/onlyoffice 
+
+##########################################################################################################################################################
+# Démarrage du Conteneur #
+##########################
+docker start $CONTENEUR
+
+##########################################################################################################################################################
+# Code Retour en Fermeture #
+############################
+exit 0

c9883404fade32189646242ff30d3b938f813006
Marc <teste74@hotmail.fr>
2023-02-25 23:17:49 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/Nextcloud/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Nextcloud/CONTROL/post-install.sh
@@ -43,25 +43,24 @@ docker container rm -f $CONTENEUR   2>/dev/null;
 ##########################################################################################################################################################
 # Création du Conteneur #
 #########################
-docker create -i -t \
---name=$CONTENEUR \
---net=bridge \
---hostname $CONTENEUR \
---restart $RESTART \
---env NEXTCLOUD_TRUSTED_DOMAINS="$NEXTCLOUD_DOM $IP_NAS"       \
---env MYSQL_HOST=${IP_NAS}:${SQL_PORT}                         \
---env MYSQL_DATABASE=$SQL_DB                                   \
---env MYSQL_USER=$SQL_USER                                     \
---env MYSQL_PASSWORD=$SQL_PASS                                 \
---env NEXTCLOUD_ADMIN_USER=$NEXTCLOUD_USER                     \
---env NEXTCLOUD_ADMIN_PASSWORD=$NEXTCLOUD_PASS                 \
---volume /volume1/Docker/${CONTENEUR}/cfg:/var/www/html/config \
---publish $PORT:80 \
+docker create -i -t                                                  \
+--name=$CONTENEUR                                                    \
+--net=bridge                                                         \
+--hostname $CONTENEUR                                                \
+--restart $RESTART                                                   \
+--env NEXTCLOUD_TRUSTED_DOMAINS="$NEXTCLOUD_DOM $IP_NAS"             \
+--env MYSQL_HOST=${IP_NAS}:${SQL_PORT}                               \
+--env MYSQL_DATABASE=$SQL_DB                                         \
+--env MYSQL_USER=$SQL_USER                                           \
+--env MYSQL_PASSWORD=$SQL_PASS                                       \
+--env NEXTCLOUD_ADMIN_USER=$NEXTCLOUD_USER                           \
+--env NEXTCLOUD_ADMIN_PASSWORD=$NEXTCLOUD_PASS                       \
+--volume /volume1/Docker/${CONTENEUR}/apps:/var/www/html/custom_apps \
+--volume /volume1/Docker/${CONTENEUR}/themes:/var/www/html/themes    \
+--volume /volume1/Docker/${CONTENEUR}/cfg:/var/www/html/config       \
+--volume /volume1/Docker/${CONTENEUR}/data:/var/www/html/data        \
+--publish $PORT:80                                                   \
 $IMAGE:latest
-
-#--volume /volume1/Docker/${CONTENEUR}/apps:/var/www/html/custom_apps \
-#--volume /volume1/Docker/${CONTENEUR}/data:/var/www/html/data        \
-#--volume /volume1/Docker/${CONTENEUR}/themes:/var/www/html/themes    \
 
 ##########################################################################################################################################################
 # Lancement du Conteneur #

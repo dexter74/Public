00bb706c6e7bc7a1f9a2b42a895726d733cf4ae0
Marc <teste74@hotmail.fr>
2023-02-25 03:22:47 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/Nextcloud/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Nextcloud/CONTROL/post-install.sh
@@ -52,7 +52,7 @@ docker create -i -t \
 --env NEXTCLOUD_ADMIN_PASSWORD=$NEXTCLOUD_PASS \
 --env NEXTCLOUD_DATA_DIR=/var/www/html/data \
 --volume /volume1/Docker/$CONTENEUR:/var/www/html \
---volume volume1/Docker/$CONTENEUR/data:/var/www/html/data \
+--volume /volume1/Docker/$CONTENEUR/data:/var/www/html/data \
 --publish $PORT:80 \
 $IMAGE:latest
 

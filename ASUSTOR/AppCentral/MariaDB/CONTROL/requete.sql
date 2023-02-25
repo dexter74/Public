921c4e13df51e1e4246648ca5dfaf4ca661ec8a6
Marc <teste74@hotmail.fr>
2023-02-25 17:17:29 +0100

    Update requete.sql

--- ASUSTOR/AppCentral/MariaDB/CONTROL/requete.sql
+++ ASUSTOR/AppCentral/MariaDB/CONTROL/requete.sql
@@ -1,12 +1,26 @@
-/* Création des Bases de données */
+/* Suppression des Databases */
+DROP SCHEMA IF EXISTS bitwarden;
+DROP SCHEMA IF EXISTS nextcloud;
+
+/* Suppression des Utilisateurs */
+DROP USER IF EXISTS bitwarden;
+DROP USER IF EXISTS nextcloud;
+
+/* Création Des Base de données */
 CREATE DATABASE bitwarden;
 CREATE DATABASE nextcloud;
 
-/* Création des utilisateurs (USER et MDP) */
-
+/* Création Utilisateurs */
 CREATE USER 'bitwarden'@'%' IDENTIFIED BY 'bitwarden';
 CREATE USER 'nextcloud'@'%' IDENTIFIED BY 'nextcloud';
 
 /* Edtiion des Permissions */
 GRANT ALL PRIVILEGES ON `bitwarden`.* TO `bitwarden`@`%` WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON `nextcloud`.* TO `nextcloud`@`%` WITH GRANT OPTION;
+
+
+/* SELECT user,host FROM mysql.user; %/
+/* SHOW DATABASES; */
+/* SHOW SCHEMAS;   */
+/* SELECT user FROM mysql.user; */
+ 

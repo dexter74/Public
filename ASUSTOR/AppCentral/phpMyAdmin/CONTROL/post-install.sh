8cb521281e8c5b9cf42961e99fae8916dba9b30b
Marc <teste74@hotmail.fr>
2023-02-25 04:33:10 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/phpMyAdmin/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/phpMyAdmin/CONTROL/post-install.sh
@@ -32,6 +32,7 @@ docker create -i -t \
 --env PMA_USER="$MYSQL_USER" \
 --env PMA_PASSWORD="$MYSQL_PASS" \
 --env PMA_PMADB="$PHPMYADMIN_DATABASE" \
+--volume /share/Docker/$CONTENEUR/:/etc/phpmyadmin/ \
 --publish $PORT \
 $IMAGE
 

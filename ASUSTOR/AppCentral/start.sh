74ee6f8f8308029c9ee26c8efc5d567f178657f4
Marc <teste74@hotmail.fr>
2023-02-25 14:53:15 +0100

    Update start.sh

--- ASUSTOR/AppCentral/start.sh
+++ ASUSTOR/AppCentral/start.sh
@@ -8,19 +8,19 @@
 
 echo "##############################"
 echo "# Container Ready for Deploy #"
-echo "  > AdGuardHome"
+echo "# > AdGuardHome              #"
 cd ~/APPZ/ASUSTOR/AppCentral/AdGuardHome        && apkg-tools_py3.py create .
-echo "  > BitWarden"
+echo "# > BitWarden                #"
 cd ~/APPZ/ASUSTOR/AppCentral/Bitwarden          && apkg-tools_py3.py create .
-echo "  > Dozzle"
+echo "# > Dozzle                   #"
 cd ~/APPZ/ASUSTOR/AppCentral/Dozzle             && apkg-tools_py3.py create .
 #cd ~/APPZ/ASUSTOR/AppCentral/Emby               && apkg-tools_py3.py create .
 #cd ~/APPZ/ASUSTOR/AppCentral/Flaresolverr       && apkg-tools_py3.py create .
 #cd ~/APPZ/ASUSTOR/AppCentral/Jackett            && apkg-tools_py3.py create .
 #cd ~/APPZ/ASUSTOR/AppCentral/Jellyfin           && apkg-tools_py3.py create .
-echo "  > MariaDB"
+echo "# > MariaDB                  #"
 cd ~/APPZ/ASUSTOR/AppCentral/MariaDB            && apkg-tools_py3.py create .
-echo "  > Nginx Reverse Proxy"
+echo "# > Nginx Reverse Proxy      #"
 cd ~/APPZ/ASUSTOR/AppCentral/NginxReverseProxy  && apkg-tools_py3.py create .
 #cd ~/APPZ/ASUSTOR/AppCentral/phpMyAdmin         && apkg-tools_py3.py create .
 #cd ~/APPZ/ASUSTOR/AppCentral/QBittorrent        && apkg-tools_py3.py create .
@@ -41,10 +41,10 @@ cd ~/APPZ/ASUSTOR/AppCentral/NginxReverseProxy  && apkg-tools_py3.py create .
 #cd ~/APPZ/ASUSTOR/AppCentral/Portainer          && apkg-tools_py3.py create .
 #cd ~/APPZ/ASUSTOR/AppCentral/Visual_Studio      && apkg-tools_py3.py create .
 
-########################################################################################
-# Container in Testing
+echo "##############################"
+echo "Container in Testing"
 #cd ~/APPZ/ASUSTOR/AppCentral/Wireguard          && apkg-tools_py3.py create .
-echo "  > MKcert"
+echo "# > MKcert                   #"
 cd ~/APPZ/ASUSTOR/AppCentral/mkcert              && apkg-tools_py3.py create .
 ########################################################################################
 # Container Edit and Ready for Deploy

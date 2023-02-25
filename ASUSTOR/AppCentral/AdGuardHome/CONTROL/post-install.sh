07e3272329537d6350a55075a36bd8f2dda1ed31
Marc <teste74@hotmail.fr>
2023-02-25 15:52:10 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/AdGuardHome/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/AdGuardHome/CONTROL/post-install.sh
@@ -18,23 +18,24 @@ docker container rm -f $CONTENEUR
 docker create -i -t \
 --name=$CONTENEUR \
 --hostname $CONTENEUR \
+--net=bridge \
 --restart $RESTART \
 --env TZ="Europe/Paris" \
 --volume /usr/builtin/etc/certificate:/sslcerts/:ro \
 --volume /etc/localtime:/etc/localtime:ro \
 --volume /volume1/Docker/$CONTENEUR/conf:/opt/adguardhome/conf \
 --volume /volume1/Docker/$CONTENEUR/work:/opt/adguardhome/work \
---volume /volume1/Docker/mkcert:/cert \
 --publish 3000:3000/tcp \
---publish 9000:80/tcp   \
 --publish 3272:3272     \
---publish 53:53/tcp     \
---publish 53:53/udp     \
+
 $IMAGE:latest
 
+#--publish 9000:80/tcp   \
+#--volume /volume1/Docker/mkcert:/cert \
 #--publish 443:443/tcp   \
 #--publish 443:443/udp   \
-
+#--publish 53:53/tcp     \
+#--publish 53:53/udp     \
 ##########################################################################################################################################################
 # Lancement du Conteneur #
 ##########################

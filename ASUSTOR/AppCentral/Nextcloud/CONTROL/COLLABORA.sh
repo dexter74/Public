61f5845749fe1695e6c4b9d30f8c908a3750860b
Marc <teste74@hotmail.fr>
2023-02-25 03:42:05 +0100

    Update COLLABORA.sh

--- ASUSTOR/AppCentral/Nextcloud/CONTROL/COLLABORA.sh
+++ ASUSTOR/AppCentral/Nextcloud/CONTROL/COLLABORA.sh
@@ -8,32 +8,17 @@ clear;
 ##########################################################################################################################################################
 # Déclaration des variables #
 #############################
-CONTENEUR=Nextcloud
-IMAGE=nextcloud
+CONTENEUR=
+IMAGE=
 RESTART=unless-stopped
-PORT=1120
+PORT=1121
 
 ##########################################################################################################################################################
 # Configuration des accès Nextcloud #
 #####################################
 USERNAME=$(grep "1000" /etc/passwd | cut -d ":" -f 1)
 IP_NAS=$(ip add  | grep 192.168.1 | cut -d "/" -f 1 | cut -c 10-30)
-NEXTCLOUD_USER="admin"
-NEXTCLOUD_PASS="admin"
-NEXTCLOUD_DOM="cloud74.ddns.net"
 
-##########################################################################################################################################################
-# Configuration de la connexion à la BDD #
-##########################################
-SQL_PORT="3306"
-SQL_DB="nextcloud"
-SQL_USER="nextcloud"
-SQL_PASS="nextcloud"
-
-##########################################################################################################################################################
-# Permissions #
-###############
-# chown 33:33 -R /volume1/Docker/${CONTENEUR}
 
 ##########################################################################################################################################################
 # Fermeture du Conteneur #
@@ -47,17 +32,7 @@ docker create -i -t \
 --name=$CONTENEUR \
 --hostname $CONTENEUR \
 --restart $RESTART \
---env NEXTCLOUD_TRUSTED_DOMAINS="$NEXTCLOUD_DOM $IP_NAS" \
---env MYSQL_HOST=${IP_NAS}:${SQL_PORT} \
---env MYSQL_DATABASE=$SQL_DB \
---env MYSQL_USER=$SQL_USER \
---env MYSQL_PASSWORD=$SQL_PASS \
---env NEXTCLOUD_ADMIN_USER=$NEXTCLOUD_USER \
---env NEXTCLOUD_ADMIN_PASSWORD=$NEXTCLOUD_PASS \
---volume /volume1/Docker/${CONTENEUR}/apps:/var/www/html/custom_apps \
---volume /volume1/Docker/${CONTENEUR}/cfg:/var/www/html/config \
---volume /volume1/Docker/${CONTENEUR}/data:/var/www/html/data \
---volume /volume1/Docker/${CONTENEUR}/themes:/var/www/html/themes \
+--volume \
 --publish $PORT:80 \
 $IMAGE:latest
 

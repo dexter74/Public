01274920b7122ba9009de97991f400f1d4bfb234
Marc <teste74@hotmail.fr>
2023-02-25 17:19:31 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/MariaDB/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/MariaDB/CONTROL/post-install.sh
@@ -24,7 +24,6 @@ docker create -i -t \
 --env MYSQL_DATABASE="database" \
 --env MYSQL_USER="username" \
 --env MYSQL_PASSWORD="password" \
---env REMOTE_SQL="https://raw.githubusercontent.com/dexter74/Public/main/ASUSTOR/AppCentral/MariaDB/CONTROL/requete.sql" \
 --env TZ="Europe/Paris" \
 --env PUID=1000 \
 --env PGID=100 \
@@ -40,14 +39,9 @@ docker start $CONTENEUR
 ##########################################################################################################################################################
 # Création des BDD #
 ####################
-cp /volume1/.@plugins/AppCentral/MariaDB/CONTROL/requete.sql /volume1/Docker/$CONTENEUR
+mv requete.sql /volume1/Docker/$CONTENEUR;
 docker exec -it MariaDB sh -c 'mysql -h 127.0.0.1 -uroot -proot < /config/requete.sql'
 
-docker exec -it MariaDB mysql -uroot -proot -e "CREATE USER 'bitwarden'@'%' IDENTIFIED BY 'bitwarden';"
-docker exec -it MariaDB mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON `bitwarden`.* TO `bitwarden`@`%` WITH GRANT OPTION;"
-docker exec -it MariaDB mysql -uroot -proot -e "CREATE DATABASE nextcloud'@;"
-docker exec -it MariaDB mysql -uroot -proot -e "CREATE USER 'nextcloud'@'%' IDENTIFIED BY 'nextcloud';"
-docker exec -it MariaDB mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON `nextcloud`.* TO `nextcloud`@`%` WITH GRANT OPTION;"
 ##########################################################################################################################################################
 # Code Retour en Fermeture #
 ############################

526e4df82faf60af23a13bc72384b5ba5b8bec72
Marc <teste74@hotmail.fr>
2023-02-25 16:20:09 +0100

    Update requet.sql

--- ASUSTOR/AppCentral/MariaDB/CONTROL/requet.sql
+++ ASUSTOR/AppCentral/MariaDB/CONTROL/requet.sql
@@ -1,1 +1,12 @@
+/* Création des Bases de données */
+CREATE DATABASE bitwarden;
+CREATE DATABASE nextcloud;
 
+/* Création des utilisateurs */
+CREATE USER 'bitwarden'@'%' IDENTIFIED BY 'bitwarden';
+CREATE USER 'nextcloud'@'%' IDENTIFIED BY 'nextcloud';
+
+
+/* Edtiion des Permissions */
+GRANT ALL PRIVILEGES ON `bitwarden`.* TO `bitwarden`@`%` WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `nextcloud`.* TO `nextcloud`@`%` WITH GRANT OPTION;

316d03895d3d4357df937c3de883f38eb2f1ab94
Marc <teste74@hotmail.fr>
2023-03-20 04:58:39 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/Wireguard/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Wireguard/CONTROL/post-install.sh
@@ -1,19 +1,49 @@
 #!/bin/sh
 
 ##########################################################################################################################################################
-# TEST #
-########
-APP_NAME=/usr/local/AppCentral/Wireguard2/
-APKG_TEMP_DIR=/tmp/
-
-##########################################################################################################################################################
 # Déclaration de variable #
 ###########################
 IMAGE=linuxserver/wireguard
 CONTENEUR=wireguard
-RESTART=unless-stopped
 PORT=51820
 
+##############################################################################################################################
+# Wireguard sous Docker #
+#########################
+CONTENEUR=wireguard
+IMAGE=linuxserver/wireguard
+RESTART=unless-stopped
+PORT=51820
+#
+##############################################################################################################################
+# Récupération des informations #
+#################################
+# Récupération du nom de l'interface active
+WIREGUARD_NET=$(ip add | grep -A2 -v "ff:ff\|DOWN\|valid\|lo\|dock" | grep "global" | cut -d "l" -f 3 | cut -c 2-50)
+#
+# Récupération de l'adresse IPV4
+WIREGUARD_HOST=$(ip add show $IF_PHYSIC | grep inet | cut -d "/" -f 1 | cut -d "t" -f 2 | cut -c 2-50)
+#
+##############################################################################################################################
+# Configuration Wireguard - Serveur #
+#####################################
+WIREGUARD_ADDR=10.0.0.1/24
+WIREGUARD_PORT=$PORT
+WIREGUARD_PRIVATE=AA5zhrdTCOb7ISqUZF3eTwwyCfEqRC935lOn4HsR20Y=
+WIREGUARD_PUBLIC=AExeAE6MgH0ofW4WxSDLXoIuvoGyW6UPjWCUfya+Dno=
+#
+# Client 1:
+CLIENT1_PUBLIC=JXtUuxYYQE1YBJsxiAiG2EdAJs/5cXdLKcX/J/3T/Vk=
+CLIENT1_ALLOWIP=10.0.0.2/32
+#
+# Client 2:
+CLIENT2_PUBLIC=9PM8zGiDKWwjZgxQC3yygFPer9aO58DtQCeM3HfyHTY=
+CLIENT2_ALLOWIP=10.0.0.3/32
+#
+# Client 3:
+CLIENT3_PUBLIC=O18/9fKBQoNLvRlw9C9iZU9gHeR67++8/NNaC7nfT00=
+CLIENT3_ALLOWIP=10.0.0.4/32
+#
 ##########################################################################################################################################################
 # Fermeture du Conteneur #
 ##########################

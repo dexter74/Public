c2bdbb331df2606b39a084958dfe1280b7d11181
Marc <teste74@hotmail.fr>
2023-03-20 04:24:12 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/Wireguard/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Wireguard/CONTROL/post-install.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 
+IMAGE=linuxserver/wireguard
+CONTENEUR=wireguard
+RESTART=unless-stopped
+PORT=51820
+##########################################################################################################################################################
+# Fermeture du Conteneur #
+##########################
+docker container rm -f $CONTENEUR;
+
+##########################################################################################################################################################
+# Création du Conteneur #
+#########################
+docker create -i -t \
+--name=$CONTENEUR \
+--restart $RESTART \
+--net=bridge \
+--hostname $CONTENEUR \
+--cap-add=NET_ADMIN \
+--cap-add=SYS_MODULE \
+--env PUID=1000 \
+--env PGID=100 \
+--env TZ=Etc/PARIS \
+--publish $PORT \
+$IMAGE:latest
+
 ##########################################################################################################################################################
 # Code Retour en Fermeture #
 ############################

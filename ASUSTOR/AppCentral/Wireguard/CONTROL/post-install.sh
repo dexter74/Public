bc384c58a79efa346982d90cb60bcf6a6c12bfc6
Marc <teste74@hotmail.fr>
2023-03-20 05:00:42 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/Wireguard/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Wireguard/CONTROL/post-install.sh
@@ -44,6 +44,52 @@ CLIENT2_ALLOWIP=10.0.0.3/32
 CLIENT3_PUBLIC=O18/9fKBQoNLvRlw9C9iZU9gHeR67++8/NNaC7nfT00=
 CLIENT3_ALLOWIP=10.0.0.4/32
 #
+##############################################################################################################################
+
+
+
+
+
+##############################################################################################################################
+# Génération de la configuration de Wireguard #
+###############################################
+# Création du Dossier
+rm -r /etc/wireguard 2>/dev/null;
+mkdir /etc/wireguard 2>/dev/null;
+
+# Clés Public et Privée
+echo "$WIREGUARD_PUBLIC" > /etc/wireguard/publickey
+echo "$WIREGUARD_PRIVATE" > /etc/wireguard/privatekey
+
+echo "################################################################################################################
+# Configuration Serveur #
+#########################
+[Interface]
+Address = $WIREGUARD_ADDR
+ListenPort = $WIREGUARD_PORT
+PrivateKey = $WIREGUARD_PRIVATE
+################################################################################################################
+# Pare-Feu #
+############
+PostUp     = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $WIREGUARD_NET -j MASQUERADE
+PostDown   = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $WIREGUARD_NET -j MASQUERADE
+################################################################################################################
+# Clients #
+###########
+[Peer]
+PublicKey  = $CLIENT1_PUBLIC
+AllowedIPs = $CLIENT1_ALLOWIP
+[Peer]
+PublicKey  = $CLIENT2_PUBLIC
+AllowedIPs = $CLIENT2_ALLOWIP
+[Peer]
+PublicKey  = $CLIENT3_PUBLIC
+AllowedIPs = $CLIENT3_ALLOWIP
+#############################################################################################################" > /etc/wireguard/wg0.conf
+
+
+
+
 ##########################################################################################################################################################
 # Fermeture du Conteneur #
 ##########################

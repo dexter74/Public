73e5b8276136ebfa3bcdcf2bab6ff2d2646fd44e
Marc <teste74@hotmail.fr>
2023-03-20 03:01:20 +0100

    Update gen_config.sh

--- ASUSTOR/AppCentral/Wireguard/CONTROL/config/gen_config.sh
+++ ASUSTOR/AppCentral/Wireguard/CONTROL/config/gen_config.sh
@@ -3,37 +3,38 @@
 #########################################################
 source ./settings
 
-
-
 # Création du Dossier
 mkdir /etc/wireguard 2>/dev/null;
 
-# Clé Public 
-echo "" > /etc/
-
-# Clé Privée
-echo "" > /etc/
+# Clés Public et Privée
+echo "$WIREGUARD_PUBLIC" > /etc/wireguard/publickey
+echo "$WIREGUARD_PRIVATE" > /etc/wireguard/privatekey
 
 echo "
-#############################################################################################################
+################################################################################################################
 # Configuration Serveur #
 #########################
 [Interface]
-Address = $SRV_WG_ADDRESS
-ListenPort = $SRV_WG_PORT
-PrivateKey = $SRV_WG_PRIVATE
-
-#############################################################################################################
+Address = $WIREGUARD_ADDR
+ListenPort = $WIREGUARD_PORT
+PrivateKey = $WIREGUARD_PRIVATE
+################################################################################################################
 # Pare-Feu #
 ############
-PostUp     = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $INTEFACE -j MASQUERADE
-PostDown   = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $INTEFACE -j MASQUERADE
-
-#############################################################################################################
+PostUp     = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $WIREGUARD_NET -j MASQUERADE
+PostDown   = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $WIREGUARD_NET -j MASQUERADE
+################################################################################################################
 # Clients #
 ###########
 [Peer]
-PublicKey  = $CL1_WG_PUBLIC
-AllowedIPs = $CL1_WG_ALLOW
+PublicKey  = $CLIENT1_PUBLIC
+AllowedIPs = $CLIENT1_ALLOWIP
 
+[Peer]
+PublicKey  = $CLIENT2_PUBLIC
+AllowedIPs = $CLIENT2_ALLOWIP
+
+[Peer]
+PublicKey  = $CLIENT3_PUBLIC
+AllowedIPs = $CLIENT3_ALLOWIP
 #############################################################################################################" > /etc/wireguard/wg0.conf

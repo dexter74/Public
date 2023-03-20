f65209a6171313cd75a8b4cea36123e58906c0d8
Marc <teste74@hotmail.fr>
2023-03-20 05:10:06 +0100

    Update post-install.sh

--- ASUSTOR/AppCentral/Wireguard/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/Wireguard/CONTROL/post-install.sh
@@ -26,7 +26,7 @@ docker create -i -t \
 --env PUID=1000 \
 --env PGID=100 \
 --env TZ=Etc/PARIS \
---volume /etc/wireguard:/config \
+--volume /etc/wireguard/wg0.conf:/config/wg0.conf \
 --volume /sys/module/wireguard::/lib/modules \
 --publish $PORT \
 $IMAGE:latest

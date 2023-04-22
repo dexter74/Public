f1b428bacb1e2618b3e9c7fa82807dfd5496b592
Marc <teste74@hotmail.fr>
2023-04-22 02:08:37 +0200

    Update post-install.sh

--- ASUSTOR/AppCentral/mkcert/CONTROL/post-install.sh
+++ ASUSTOR/AppCentral/mkcert/CONTROL/post-install.sh
@@ -7,7 +7,7 @@ IMAGE="vishnunair/docker-mkcert"
 CONTENEUR="mkcert"
 DOMAIN="asustor.local bitwarden.local dns.local dozzle.local emby.local jackett.lan nginx.local radarr.local sonarr.local torrent.local"
 RESTART=no
-IP_NAS=$(ip add | grep 192.168.1 | cut -d "/" -f 1 | cut -c 10-30)
+IP_NAS=$(ip add | grep 192.168.0 | cut -d "/" -f 1 | cut -c 10-30)
 
 ##########################################################################################################################################################
 # Fermeture du Conteneur #

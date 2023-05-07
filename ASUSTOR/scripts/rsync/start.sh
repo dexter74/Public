112ec91547d86001bb516f4f4c88a2a75a157a6d
Marc <teste74@hotmail.fr>
2023-05-07 17:01:13 +0200

    Update start.sh

--- ASUSTOR/scripts/rsync/start.sh
+++ ASUSTOR/scripts/rsync/start.sh
@@ -10,11 +10,11 @@ source ./settings.conf
 ######################################################################################################################
 # Sauvegarde Réelle #
 #####################
-#rsync $OPTION "$DOCKER_SOURCE"     "$DOCKER_DESTINATION"     > rsync.out 2> rsync.err;
-#rsync $OPTION "$DOWNLOAD_SOURCE"   "$DOWNLOAD_DESTINATION"   > rsync.out 2> rsync.err;
-#rsync $OPTION "$HOME_SOURCE"       "$HOME_DESTINATION"       > rsync.out 2> rsync.err;
-#rsync $OPTION "$MUSIC_SOURCE"      "$MUSIC_DESTINATION"      > rsync.out 2> rsync.err;
-#rsync $OPTION "$SAUVEGARDE_SOURCE" "$SAUVEGARDE_DESTINATION" > rsync.out 2> rsync.err;
-#rsync $OPTION "$WEB_SOURCE"        "$WEB_DESTINATION"        > rsync.out 2> rsync.err;
-#rsync $OPTION "$WINDOWS_SOURCE"    "$WINDOWS_DESTINATION"    > rsync.out 2> rsync.err;
+#nohup rsync $OPTION "$DOCKER_SOURCE"     "$DOCKER_DESTINATION"     > rsync.out 2> rsync.err;
+#nohup rsync $OPTION "$DOWNLOAD_SOURCE"   "$DOWNLOAD_DESTINATION"   > rsync.out 2> rsync.err;
+#nohup rsync $OPTION "$HOME_SOURCE"       "$HOME_DESTINATION"       > rsync.out 2> rsync.err;
+#nohup rsync $OPTION "$MUSIC_SOURCE"      "$MUSIC_DESTINATION"      > rsync.out 2> rsync.err;
+#nohup rsync $OPTION "$SAUVEGARDE_SOURCE" "$SAUVEGARDE_DESTINATION" > rsync.out 2> rsync.err;
+#nohup rsync $OPTION "$WEB_SOURCE"        "$WEB_DESTINATION"        > rsync.out 2> rsync.err;
+#nohup rsync $OPTION "$WINDOWS_SOURCE"    "$WINDOWS_DESTINATION"    > rsync.out 2> rsync.err;
 ######################################################################################################################

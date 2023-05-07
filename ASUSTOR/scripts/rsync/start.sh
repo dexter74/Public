f20fb778619f493f32def67fd66d455f9717c4a4
Marc <teste74@hotmail.fr>
2023-05-07 16:57:06 +0200

    Update start.sh

--- ASUSTOR/scripts/rsync/start.sh
+++ ASUSTOR/scripts/rsync/start.sh
@@ -20,24 +20,11 @@ mkdir rapport;
 ######################################################################################################################
 # Sauvegarde Réelle #
 #####################
-nohup rsync $OPTION "$DOCKER_SOURCE"     "$DOCKER_DESTINATION"     > docker.out     2> docker.err &
-mv docker.* rapport/;
-
-nohup rsync $OPTION "$DOWNLOAD_SOURCE"   "$DOWNLOAD_DESTINATION"   > download.out   2> download.err &
-mv download.* rapport/;
-
-nohup rsync $OPTION "$HOME_SOURCE"       "$HOME_DESTINATION"       > home.out       2> home.err &
-mv home.* rapport/;
-
-nohup rsync $OPTION "$MUSIC_SOURCE"      "$MUSIC_DESTINATION"      > music.out      2> music.err &
-mv music.* rapport/;
-
-nohup rsync $OPTION "$SAUVEGARDE_SOURCE" "$SAUVEGARDE_DESTINATION" > sauvegarde.out 2> sauvegarde.err &
-mv sauvegarde.* rapport/;
-
-nohup rsync $OPTION "$WEB_SOURCE"        "$WEB_DESTINATION"        > web.out        2> web.err &
-mv web.* rapport/;
-
-nohup rsync $OPTION "$WINDOWS_SOURCE"    "$WINDOWS_DESTINATION"    > windows.out    2> windows.err &
-mv windows.* rapport/;
+nohup rsync $OPTION "$DOCKER_SOURCE"     "$DOCKER_DESTINATION"     > rsync.out 2> rsync.err &
+nohup rsync $OPTION "$DOWNLOAD_SOURCE"   "$DOWNLOAD_DESTINATION"   > rsync.out 2> rsync.err &
+nohup rsync $OPTION "$HOME_SOURCE"       "$HOME_DESTINATION"       > rsync.out 2> rsync.err &
+nohup rsync $OPTION "$MUSIC_SOURCE"      "$MUSIC_DESTINATION"      > rsync.out 2> rsync.err &
+nohup rsync $OPTION "$SAUVEGARDE_SOURCE" "$SAUVEGARDE_DESTINATION" > rsync.out 2> rsync.err &
+nohup rsync $OPTION "$WEB_SOURCE"        "$WEB_DESTINATION"        > rsync.out 2> rsync.err &
+nohup rsync $OPTION "$WINDOWS_SOURCE"    "$WINDOWS_DESTINATION"    > rsync.out 2> rsync.err &
 ######################################################################################################################

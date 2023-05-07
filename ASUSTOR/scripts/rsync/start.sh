a4368f3fccedc18dcdfa9f69023088bdb504c955
Marc <teste74@hotmail.fr>
2023-05-07 16:46:36 +0200

    Update start.sh

--- ASUSTOR/scripts/rsync/start.sh
+++ ASUSTOR/scripts/rsync/start.sh
@@ -10,10 +10,12 @@ exit;
 # NAS Vers MyArchives1 #
 ########################
 
-#rsync $OPTION "$DOCKER_SOURCE"     "$DOCKER_DESTINATION";
-#rsync $OPTION "$DOWNLOAD_SOURCE"   "$DOWNLOAD_DESTINATION";
-#rsync $OPTION "$HOME_SOURCE"       "$HOME_DESTINATION";
-#rsync $OPTION "$MUSIC_SOURCE"      "$MUSIC_DESTINATION";
-#rsync $OPTION "$SAUVEGARDE_SOURCE" "$SAUVEGARDE_DESTINATION";
-#rsync $OPTION "$WEB_SOURCE"        "$WEB_DESTINATION";
-#rsync $OPTION "$WINDOWS_SOURCE"    "$WINDOWS_DESTINATION";
+#nohup rsync $OPTION "$DOCKER_SOURCE"     "$DOCKER_DESTINATION"     > myprogram.out 2> myprogram.err &
+#nohup rsync $OPTION "$DOWNLOAD_SOURCE"   "$DOWNLOAD_DESTINATION"   > myprogram.out 2> myprogram.err &
+#nohup rsync $OPTION "$HOME_SOURCE"       "$HOME_DESTINATION"       > myprogram.out 2> myprogram.err &
+#nohup rsync $OPTION "$MUSIC_SOURCE"      "$MUSIC_DESTINATION"      > myprogram.out 2> myprogram.err &
+#nohup rsync $OPTION "$SAUVEGARDE_SOURCE" "$SAUVEGARDE_DESTINATION" > myprogram.out 2> myprogram.err &
+#nohup rsync $OPTION "$WEB_SOURCE"        "$WEB_DESTINATION"        > myprogram.out 2> myprogram.err &
+#nohup rsync $OPTION "$WINDOWS_SOURCE"    "$WINDOWS_DESTINATION"    > myprogram.out 2> myprogram.err &
+
+rsync -avu --progress --delete $SOURCE $DESTINATION 
